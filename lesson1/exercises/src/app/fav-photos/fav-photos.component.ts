24d2486cee716dea890d912a53afe2b03846c6f4
Justin Griswold <theeTGP@gmail.com>
2021-12-06 13:58:21 -0600

    part one completed

--- lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
+++ lesson1/exercises/src/app/fav-photos/fav-photos.component.ts
@@ -6,10 +6,10 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./fav-photos.component.css']
 })
 export class FavPhotosComponent implements OnInit {
-  photosTitle = 'Photo/Image Section Title Here';
-  image1 = 'https://www.launchcode.org/assets/icons/target-00c7a0bc7776901e3c5cdc4c36c465f19e41a249bc7267a595f0bfa14ab944b7.png';
-  image2 = '';
-  image3 = '';
+  photosTitle = 'Pictures or something of the sort';
+  image1 = 'https://icdn.digitaltrends.com/image/digitaltrends/tmnt_secret_of_the_ooze_9-2-720x720.jpg';
+  image2 = 'https://bbts1.azureedge.net/images/p/full/2020/10/07490a8f-220f-4c56-abe8-2dc70f84aea4.jpg';
+  image3 = 'https://bbts1.azureedge.net/images/p/full/2021/10/2b882830-8c6f-4f6f-b272-8c919a9930ec.jpg';
 
   constructor() { }
 
